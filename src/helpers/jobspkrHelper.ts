d1ebd696fbce9f6cf530fe6f26598485f9e672b3
md shariful Islam <eng.mdshariful.islam.7@gmail.com>
2026-01-31 18:44:59 +0600

    fix: remove console log from getJobs and handle error response

--- src/helpers/jobspkrHelper.ts
+++ src/helpers/jobspkrHelper.ts
@@ -25,7 +25,13 @@ class JobspikrHelper{
 
     async getJobs(query:IQuery,category?:string){
         const data:JobspikrResponse= await this.requestHandler('https://api.jobspikr.com/v2/data','POST',buildElasticQuery(query));
-        console.log(data);
+
+        if(data.status==='error') {
+            return {
+                data:[],
+                next_cursor:undefined
+            }
+        }
         
         
         return {
